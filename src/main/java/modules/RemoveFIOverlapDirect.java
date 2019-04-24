1b0791d6650ac52ae9fb0dfeacfcbb0f252468ef
NDTanja <tu@ids-mannheim.de>
2019-04-24 08:48:42 +0200

    Add files via upload

--- src/main/java/modules/RemoveFIOverlapDirect.java
+++ src/main/java/modules/RemoveFIOverlapDirect.java
@@ -2,16 +2,12 @@ package modules;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.Type;
 import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.cas.text.AnnotationIndex;
-import org.apache.uima.fit.util.CasUtil;
 
 import util.Util;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -34,16 +30,9 @@ public class RemoveFIOverlapDirect {
 
     public CAS process(CAS mainCas) {
 
-        String tokenKind = "no_cab";
-        // first, remove all annotations of this type that are already in the document
-        //mainCas = Util.removeAllAnnotationsOfType(mainCas, "de.idsma.rw.Stwr");
-
         Type stwrType = mainCas.getTypeSystem().getType("de.idsma.rw.Stwr");
         Type dirType = mainCas.getTypeSystem().getType("de.idsma.rw.rule.RuleDirect");
-        Type tokenType = mainCas.getTypeSystem().getType("de.idsma.rw.preprocessing.Token");
 
-        // CabToken annotation
-        Type cabTokenType = mainCas.getTypeSystem().getType("de.idsma.rw.CabToken");
 
         //create a list with all fi annotations
         AnnotationIndex<AnnotationFS> annotationIndex = mainCas.getAnnotationIndex(stwrType);
@@ -71,10 +60,6 @@ public class RemoveFIOverlapDirect {
                 }
             }
         }
-
-
-
-
         return mainCas;
     }
 }
